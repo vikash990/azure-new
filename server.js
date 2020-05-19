7363a2ac249645cceed1c59f20c8eb78d56b2f60
vikash990 <vikashadubey>
2020-05-19 13:27:30 +0530

    production

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ var cors =require('cors');
 var bodyParser=require('body-parser');
 var app=express();
 const mongoose=require('mongoose');
-var port=process.env.PORT||5000;
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+var port=5000||process.env.PORT;
 
 const path = require("path");
 
@@ -14,14 +19,24 @@ app.use(
         extended:false
     })
 )
-const mongoURI='mongodb+srv://devikTech:9931320688v@cluster0-koncr.mongodb.net/test?retryWrites=true&w=majority';
+
+console.log(process.env.port);
+
+/* const mongoURI='mongodb+srv://devikTech:9931320688v@cluster0-koncr.mongodb.net/test?retryWrites=true&w=majority';  */
+
+const mongoURI=process.env.mongoURI;
+
+console.log(mongoURI)
+
+
 mongoose.connect(
-    mongoURI,
+  mongoURI,
     {
         useNewUrlParser:true
     }
 ).then(()=>console.log('MongoDb Connected')).catch(err=>console.log(err))
 
+console.log(process.env.NODE_ENV)
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build")); // change this if your dir structure is different
