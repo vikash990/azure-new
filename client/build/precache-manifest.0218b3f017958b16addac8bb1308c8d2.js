80375c65edf3641b0bf5f61f809642e210b74545
vikash990 <vikashadubey>
2020-05-23 00:23:34 +0530

    build

---
+++ client/build/precache-manifest.0218b3f017958b16addac8bb1308c8d2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "888ace947cb5613fbdc83c0d03013311",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8ca856e61e8e43b3c1f",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d4e5243715734dd57965",
+    "url": "/static/css/main.d13c399a.chunk.css"
+  },
+  {
+    "revision": "f8ca856e61e8e43b3c1f",
+    "url": "/static/js/2.658e1377.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.658e1377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4e5243715734dd57965",
+    "url": "/static/js/main.98402c95.chunk.js"
+  },
+  {
+    "revision": "3fcab70e042d81ffea78",
+    "url": "/static/js/runtime-main.0548e3b8.js"
+  },
+  {
+    "revision": "176cc6c05df89554c04d02f2d0a77d96",
+    "url": "/static/media/angle_brackets.176cc6c0.jpg"
+  },
+  {
+    "revision": "5fe1d31f86ef4029cee0e239fff3e57f",
+    "url": "/static/media/deviktech.5fe1d31f.png"
+  },
+  {
+    "revision": "f77da8f1b9bf13c536e8dc220c71a72b",
+    "url": "/static/media/ds_algo_logo.f77da8f1.png"
+  }
+]);
